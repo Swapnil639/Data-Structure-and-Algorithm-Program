a9b6953764b8c5d6f46f1db6afa8bc187dc3fca0
Swapnil Patil <102506228+Swapnil639@users.noreply.github.com>
2023-04-22 10:12:24 +0530

    Update PrimeNumber.java

--- src/com/bridgelabz/algorithm/PrimeNumber.java
+++ src/com/bridgelabz/algorithm/PrimeNumber.java
@@ -6,21 +6,26 @@ public class PrimeNumber {
     static Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) {
-        System.out.println("Enter the Number");
-        int number = scanner.nextInt();
-        primeNumber(number);
+        PrimeNumber prime = new PrimeNumber();
+        prime.primeMethod();
     }
 
-    static void primeNumber(int number) {
-        int temp = 0;
-        for (int i = 2; i < number; i++) {
-            if (number % i == 0) {
-                temp = temp + 1;
+    public void primeMethod() {
+        System.out.println("PrimeNumber series is :");
+        System.out.println("Enter the lower limit");
+        int a = scanner.nextInt();
+        System.out.println("Enter the upper limit");
+        int b = scanner.nextInt();
+        for (int i = a; i < b; i++) {
+            int count = 0;
+            for (int j = 2; j <= i / 2; j++) {
+                if (i % j == 0) {
+                    count++;
+                }
+            }
+            if (count == 0) {
+                System.out.print(i + " ");
             }
         }
-        if (temp > 0)
-            System.out.println(number + " is not prime number");
-        else
-            System.out.println(number + " is prime number");
     }
 }
